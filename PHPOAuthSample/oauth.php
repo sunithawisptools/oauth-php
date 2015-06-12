4829b676ca6a9b805f4d3b4094f716b67e1aa988
skodamarthi <f2004156@gmail.com>
2015-06-12 08:39:05 +0100

    Update oauth.php

--- PHPOAuthSample/oauth.php
+++ PHPOAuthSample/oauth.php
@@ -7,35 +7,24 @@ define('OAUTH_ACCESS_URL', 'https://oauth.intuit.com/oauth/v1/get_access_token')
 define('OAUTH_AUTHORISE_URL', 'https://appcenter.intuit.com/Connect/Begin');
 
 // The url to this page. it needs to be dynamic to handle runnable's dynamic urls
-$var = $_SERVER['HTTP_HOST'];
 
 define('CALLBACK_URL','http://'.$_SERVER['HTTP_HOST'].'/PHPOAuthSample/oauth.php');
-echo $var = CALLBACK_URL;
-echo "Callback url is $var";
 // cleans out the token variable if comming from
 // connect to QuickBooks button
 if ( isset($_GET['start'] ) ) {
-  echo "<br /> Session token cleared! <br />";
   unset($_SESSION['token']);
 }
  
 try {
-echo ("About to start oAuth \n");
   $oauth = new OAuth( OAUTH_CONSUMER_KEY, OAUTH_CONSUMER_SECRET, OAUTH_SIG_METHOD_HMACSHA1, OAUTH_AUTH_TYPE_URI);
-  echo ("Created oAuth object \n");
   $oauth->enableDebug();
   $oauth->disableSSLChecks(); //To avoid the error: (Peer certificate cannot be authenticated with given CA certificates)
   if (!isset( $_GET['oauth_token'] ) && !isset($_SESSION['token']) ){
 	// step 1: get request token from Intuit
-	  echo ("About to call getRequestToken \n");
     $request_token = $oauth->getRequestToken( OAUTH_REQUEST_URL, CALLBACK_URL );
-	 echo ("Got RequestToken \n");
 		$_SESSION['secret'] = $request_token['oauth_token_secret'];
 		// step 2: send user to intuit to authorize 
 		header('Location: '. OAUTH_AUTHORISE_URL .'?oauth_token='.$request_token['oauth_token']);
-		$oauthToken = $request_token['oauth_token'];
-		$oauthSecret = $request_token['oauth_token_secret'];;
-		echo "Successfully obtained oauth token and secret. Token is $oauthToken and secret is $oauthSecret";
 	}
 	
 	if ( isset($_GET['oauth_token']) && isset($_GET['oauth_verifier']) ){
@@ -51,7 +40,6 @@ echo ("About to start oAuth \n");
 	 $realmId = $_SESSION['realmId'];
 	 $dataSource = $_SESSION['dataSource'];
 	 $secret = $_SESSION['secret'] ;
-   // echo " Token is $token , realmID is $realmId, secret is $secret and DS is $dataSource";
     // write JS to pup up to refresh parent and close popup
     echo '<script type="text/javascript">
             window.opener.location.href = window.opener.location.href;
